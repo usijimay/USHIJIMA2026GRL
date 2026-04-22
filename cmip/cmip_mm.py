53bcb55e4806c590e0c574d0778853fdf21f7444
usijimay <ushijima.yusuke.fp@ehime-u.ac.jp>
2026-04-22 16:00:22 +0900

    refactor: remove ns parameter from function signatures and calls

--- cmip/cmip_mm.py
+++ cmip/cmip_mm.py
@@ -126,7 +126,7 @@ def read_data3D_MM(varname, expid, ybgn, yend, models = None, cann = True):
     return models, lonem, latem, levem, varMem
 
 
-def read_datas_MM(ybgn, yend, fnc, ns, models = None, cem = False, dirbase = '/data16/theme-C/usijimay'):
+def read_datas_MM(ybgn, yend, fnc, models = None, cem = False, dirbase = '/data16/theme-C/usijimay'):
 
     if models == None:
         models = read_models()
@@ -275,17 +275,17 @@ def read_datas_MM(ybgn, yend, fnc, ns, models = None, cem = False, dirbase = '/d
     dbdzcfem = {}
     egrcfem  = {}          
     for model in models:
-        toscf0 = fnc(tosMem[model], ns)
-        uacf0 = fnc(uacMem[model], ns)
-        uaaf0 = fnc(uaaMem[model], ns)
-        vacf0 = fnc(vacMem[model], ns)
-        vaaf0 = fnc(vaaMem[model], ns)
-        wacf0 = fnc(wacMem[model], ns)
-        waaf0 = fnc(waaMem[model], ns)
-        tacf0 = ((fnc(tacMem[model], ns).T * (1.e5/levtem[model])**(Rd/Cp))).T
-        taaf0 = ((fnc(taaMem[model], ns).T * (1.e5/levtem[model])**(Rd/Cp))).T
-        zgcf0 = fnc(zgcMem[model], ns)
-        zgaf0 = fnc(zgaMem[model], ns)        
+        toscf0 = fnc(tosMem[model])
+        uacf0 = fnc(uacMem[model])
+        uaaf0 = fnc(uaaMem[model])
+        vacf0 = fnc(vacMem[model])
+        vaaf0 = fnc(vaaMem[model])
+        wacf0 = fnc(wacMem[model])
+        waaf0 = fnc(waaMem[model])
+        tacf0 = ((fnc(tacMem[model]).T * (1.e5/levtem[model])**(Rd/Cp))).T
+        taaf0 = ((fnc(taaMem[model]).T * (1.e5/levtem[model])**(Rd/Cp))).T
+        zgcf0 = fnc(zgcMem[model])
+        zgaf0 = fnc(zgaMem[model])        
             
         if latuem[model][0] != latvem[model][0]:
             vacf0 = interpolate.interp1d(latvem[model], vacf0, kind = 'linear', axis=-2, fill_value = 'extrapolate')(np.array(latuem[model]))
@@ -332,8 +332,214 @@ def read_datas_MM(ybgn, yend, fnc, ns, models = None, cem = False, dirbase = '/d
     return models, lonuem, latuem, levuem, lonvem, latvem, levvem, lonwem, latwem, levwem, lontem, lattem, levtem, toscfem, uacfem, uaafem, vacfem, vaafem, wacfem, waafem, tacfem, taafem, zgcfem, zgafem, dbdycfem, dbdyafem, dbdzcfem, dbdzafem, egrcfem, egrafem
 
 
+# def read_datas_MM(ybgn, yend, fnc, ns, models = None, cem = False, dirbase = '/data16/theme-C/usijimay'):
 
-def read_datas_MMM(ybgn, yend, fnc, ns, models = None, cintm = False, dirbase = '/data16/theme-C/usijimay'):
+#     if models == None:
+#         models = read_models()
+        
+#     R0 = 6.375e6
+#     Rd = 2.87e2
+#     Cp = 1.004e3
+#     lev0 = np.array([1.e5, 9.25e4, 8.5e4, 7.e4, 6.e4, 5.e4, 4.e4, 3.e4, 2.5e4, 2.e4, 1.5e4, 1.e4, 7.e3, 5.e3, 3.e3, 2.e3, 1.e3, 5.e2, 1.e2])
+    
+#     lonuem = {}
+#     latuem = {}
+#     levuem = {}
+#     lonvem = {}
+#     latvem = {}
+#     levvem = {}
+#     lonwem = {}
+#     latwem = {}
+#     levwem = {}
+#     lontem = {}
+#     lattem = {}
+#     levtem = {}
+#     tosMem = {}
+#     uacMem = {}
+#     vacMem = {}
+#     wacMem = {}
+#     zgcMem = {}
+#     tacMem = {}
+#     uaaMem = {}
+#     vaaMem = {}
+#     waaMem = {}
+#     taaMem = {}
+#     zgaMem = {}
+
+#     nmdls = np.size(models)
+#     for nmdl in range(nmdls):
+#         model = models[nmdl]
+#         print(model)
+
+#         lono, lato, tosM = ReadData2dClim('tos', model, 'historical', ybgn, yend, regrid = True, dirbase = dirbase)
+
+#         lonu, latu, levu, uacM = ReadData3dClim('ua', model, 'historical', ybgn, yend, regrid = False, dirbase = dirbase)
+#         lonv, latv, levv, vacM = ReadData3dClim('va', model, 'historical', ybgn, yend, regrid = False, dirbase = dirbase)
+#         lonw, latw, levw, wacM = ReadData3dClim('wap', model, 'historical', ybgn, yend, regrid = False, dirbase = dirbase)
+#         lont, latt, levt, tacM = ReadData3dClim('ta', model, 'historical', ybgn, yend, regrid = False, dirbase = dirbase)
+#         lont, latt, levt, zgcM = ReadData3dClim('zg', model, 'historical', ybgn, yend, regrid = False, dirbase = dirbase)
+
+#         lonu, latu, levu, uaaM = ReadData3dClim('ua', model, 'amip', ybgn, yend, regrid = False, dirbase = dirbase)
+#         lonv, latv, levv, vaaM = ReadData3dClim('va', model, 'amip', ybgn, yend, regrid = False, dirbase = dirbase)
+#         lonw, latw, levw, waaM = ReadData3dClim('wap', model, 'amip', ybgn, yend, regrid = False, dirbase = dirbase)
+#         lont, latt, levt, taaM = ReadData3dClim('ta', model, 'amip', ybgn, yend, regrid = False, dirbase = dirbase)
+#         lont, latt, levt, zgaM = ReadData3dClim('zg', model, 'amip', ybgn, yend, regrid = False, dirbase = dirbase)
+        
+#         lonuem[model] = lonu
+#         latuem[model] = latu
+#         levuem[model] = levu
+#         lonvem[model] = lonv
+#         latvem[model] = latv
+#         levvem[model] = levv
+#         lonwem[model] = lonw
+#         latwem[model] = latw
+#         levwem[model] = levw
+#         lontem[model] = lont
+#         lattem[model] = latt
+#         levtem[model] = levt
+#         tosMem[model] = tosM
+#         uacMem[model] = uacM
+#         vacMem[model] = vacM
+#         wacMem[model] = wacM
+#         tacMem[model] = tacM
+#         zgcMem[model] = zgcM
+#         uaaMem[model] = uaaM
+#         vaaMem[model] = vaaM
+#         waaMem[model] = waaM
+#         taaMem[model] = taaM
+#         zgaMem[model] = zgaM
+
+#         if cem:
+#             if nmdl == 0:
+#                 tosMM = tosM.copy()/nmdls
+#                 uacMM = intp3D(lonu, latu, levu, uacM, lono, lato, lev0)/nmdls
+#                 vacMM = intp3D(lonv, latv, levv, vacM, lono, lato, lev0)/nmdls
+#                 wacMM = intp3D(lonw, latw, levw, wacM, lono, lato, lev0)/nmdls
+#                 tacMM = intp3D(lont, latt, levt, tacM, lono, lato, lev0)/nmdls
+#                 zgcMM = intp3D(lont, latt, levt, zgcM, lono, lato, lev0)/nmdls
+#                 uaaMM = intp3D(lonu, latu, levu, uaaM, lono, lato, lev0)/nmdls
+#                 vaaMM = intp3D(lonv, latv, levv, vaaM, lono, lato, lev0)/nmdls
+#                 waaMM = intp3D(lonw, latw, levw, waaM, lono, lato, lev0)/nmdls
+#                 taaMM = intp3D(lont, latt, levt, taaM, lono, lato, lev0)/nmdls
+#                 zgaMM = intp3D(lont, latt, levt, zgaM, lono, lato, lev0)/nmdls
+#             else:
+#                 tosMM = tosMM + tosM.copy()/nmdls
+#                 uacMM = uacMM + intp3D(lonu, latu, levu, uacM, lono, lato, lev0)/nmdls
+#                 vacMM = vacMM + intp3D(lonu, latv, levv, vacM, lono, lato, lev0)/nmdls
+#                 wacMM = wacMM + intp3D(lonw, latw, levw, wacM, lono, lato, lev0)/nmdls
+#                 tacMM = tacMM + intp3D(lont, latt, levt, tacM, lono, lato, lev0)/nmdls
+#                 zgcMM = zgcMM + intp3D(lont, latt, levt, zgcM, lono, lato, lev0)/nmdls
+#                 uaaMM = uaaMM + intp3D(lonu, latu, levu, uaaM, lono, lato, lev0)/nmdls
+#                 vaaMM = vaaMM + intp3D(lonv, latv, levv, vaaM, lono, lato, lev0)/nmdls
+#                 waaMM = waaMM + intp3D(lonw, latw, levw, waaM, lono, lato, lev0)/nmdls
+#                 taaMM = taaMM + intp3D(lont, latt, levt, taaM, lono, lato, lev0)/nmdls
+#                 zgaMM = zgaMM + intp3D(lont, latt, levt, zgaM, lono, lato, lev0)/nmdls
+
+#     if cem:
+#         model = 'Multi-Model Mean'
+#         lonuem[model] = lono
+#         latuem[model] = lato
+#         levuem[model] = lev0
+#         lonvem[model] = lono
+#         latvem[model] = lato
+#         levvem[model] = lev0
+#         lonwem[model] = lono
+#         latwem[model] = lato
+#         levwem[model] = lev0
+#         lontem[model] = lono
+#         lattem[model] = lato
+#         levtem[model] = lev0
+#         tosMem[model] = tosMM
+#         uacMem[model] = uacMM
+#         vacMem[model] = vacMM
+#         wacMem[model] = wacMM
+#         tacMem[model] = tacMM
+#         zgcMem[model] = zgcMM
+#         uaaMem[model] = uaaMM
+#         vaaMem[model] = vaaMM
+#         waaMem[model] = waaMM
+#         taaMem[model] = taaMM
+#         zgaMem[model] = zgaMM            
+#         models = np.r_[models, [model]]
+        
+ 
+#     toscfem = {}
+#     uaafem = {}
+#     vaafem = {}
+#     waafem = {}
+#     taafem = {}
+#     zgafem = {}
+#     dbdyafem = {}
+#     dbdzafem = {}
+#     egrafem = {}  
+#     uacfem = {}
+#     vacfem = {}
+#     wacfem = {}
+#     tacfem = {}
+#     zgcfem = {}
+#     dbdycfem = {}
+#     dbdzcfem = {}
+#     egrcfem  = {}          
+#     for model in models:
+#         toscf0 = fnc(tosMem[model], ns)
+#         uacf0 = fnc(uacMem[model], ns)
+#         uaaf0 = fnc(uaaMem[model], ns)
+#         vacf0 = fnc(vacMem[model], ns)
+#         vaaf0 = fnc(vaaMem[model], ns)
+#         wacf0 = fnc(wacMem[model], ns)
+#         waaf0 = fnc(waaMem[model], ns)
+#         tacf0 = ((fnc(tacMem[model], ns).T * (1.e5/levtem[model])**(Rd/Cp))).T
+#         taaf0 = ((fnc(taaMem[model], ns).T * (1.e5/levtem[model])**(Rd/Cp))).T
+#         zgcf0 = fnc(zgcMem[model], ns)
+#         zgaf0 = fnc(zgaMem[model], ns)        
+            
+#         if latuem[model][0] != latvem[model][0]:
+#             vacf0 = interpolate.interp1d(latvem[model], vacf0, kind = 'linear', axis=-2, fill_value = 'extrapolate')(np.array(latuem[model]))
+#             vaaf0 = interpolate.interp1d(latvem[model], vaaf0, kind = 'linear', axis=-2, fill_value = 'extrapolate')(np.array(latuem[model]))
+#         if latuem[model][0] != latwem[model][0]:
+#             wacf0 = interpolate.interp1d(latwem[model], wacf0, kind = 'linear', axis=-2, fill_value = 'extrapolate')(np.array(latuem[model]))
+#             waaf0 = interpolate.interp1d(latwem[model], waaf0, kind = 'linear', axis=-2, fill_value = 'extrapolate')(np.array(latuem[model]))
+                
+#         dbdycf0 = (9.81/tacf0.T[:,1:-1] * (tacf0.T[:,2:]-tacf0.T[:,:-2])).T/(R0*np.deg2rad(lattem[model][2:]-lattem[model][:-2]))[:,np.newaxis]
+#         dbdycf0 = np.ma.concatenate([np.nan*np.ones_like(dbdycf0.T[:,:1]), dbdycf0.T, np.nan*np.ones_like(dbdycf0.T[:,-1:])], axis=1).T
+            
+#         dbdyaf0 = (9.81/taaf0.T[:,1:-1] * (taaf0.T[:,2:]-taaf0.T[:,:-2])).T/(R0*np.deg2rad(lattem[model][2:]-lattem[model][:-2]))[:,np.newaxis]
+#         dbdyaf0 = np.ma.concatenate([np.nan*np.ones_like(dbdyaf0.T[:,:1]), dbdyaf0.T, np.nan*np.ones_like(dbdyaf0.T[:,-1:])], axis=1).T
+                
+#         dbdzcf0 = (9.81/tacf0.T[:,:,1:-1] * (tacf0.T[:,:,2:]-tacf0.T[:,:,:-2])/(zgcf0.T[:,:,2:]-zgcf0.T[:,:,:-2])).T
+#         dbdzcf0 = np.ma.concatenate([np.nan*np.ones_like(dbdzcf0.T[:,:,:1]), dbdzcf0.T, np.nan*np.ones_like(dbdzcf0.T[:,:,-1:])], axis=2).T
+            
+#         dbdzaf0 = (9.81/taaf0.T[:,:,1:-1] * (taaf0.T[:,:,2:]-taaf0.T[:,:,:-2])/(zgaf0.T[:,:,2:]-zgaf0.T[:,:,:-2])).T
+#         dbdzaf0 = np.ma.concatenate([np.nan*np.ones_like(dbdzaf0.T[:,:,:1]), dbdzaf0.T, np.nan*np.ones_like(dbdzaf0.T[:,:,-1:])], axis=2).T                
+            
+#         egrcf0 = 0.31*np.abs(dbdycf0)/np.sqrt(np.maximum(dbdzcf0, 1.e-12)) * 86400.
+#         egraf0 = 0.31*np.abs(dbdyaf0)/np.sqrt(np.maximum(dbdzaf0, 1.e-12)) * 86400.
+        
+#         toscfem[model] = toscf0
+#         uaafem[model] = uaaf0
+#         vaafem[model] = vaaf0
+#         waafem[model] = waaf0
+#         taafem[model] = taaf0
+#         zgafem[model] = zgaf0
+#         dbdyafem[model] = dbdyaf0
+#         dbdzafem[model] = dbdzaf0
+#         egrafem[model] = egraf0
+        
+#         uacfem[model] = uacf0
+#         vacfem[model] = vacf0
+#         wacfem[model] = wacf0
+#         tacfem[model] = tacf0
+#         zgcfem[model] = zgcf0
+#         dbdycfem[model] = dbdycf0
+#         dbdzcfem[model] = dbdzcf0
+#         egrcfem[model] = egrcf0
+            
+
+#     return models, lonuem, latuem, levuem, lonvem, latvem, levvem, lonwem, latwem, levwem, lontem, lattem, levtem, toscfem, uacfem, uaafem, vacfem, vaafem, wacfem, waafem, tacfem, taafem, zgcfem, zgafem, dbdycfem, dbdyafem, dbdzcfem, dbdzafem, egrcfem, egrafem
+
+
+
+def read_datas_MMM(ybgn, yend, fnc, models = None, cintm = False, dirbase = '/data16/theme-C/usijimay'):
 
     if models == None:
         models = read_models()
@@ -366,19 +572,19 @@ def read_datas_MMM(ybgn, yend, fnc, ns, models = None, cintm = False, dirbase = 
     Cp = 1.004e3
             
     if cintm:            
-        toscf0 = fnc(tosMM, ns)
-        tosaf0 = fnc(toso, ns)
-    uacf0 = fnc(uacMM, ns)
-    uaaf0 = fnc(uaaMM, ns)
+        toscf0 = fnc(tosMM)
+        tosaf0 = fnc(toso)
+    uacf0 = fnc(uacMM)
+    uaaf0 = fnc(uaaMM)
     if cintm:    
-        vacf0 = fnc(vacMM, ns)
-        vaaf0 = fnc(vaaMM, ns)
-        wacf0 = fnc(wacMM, ns)
-        waaf0 = fnc(waaMM, ns)
-    tacf0 = ((fnc(tacMM, ns).T * (1.e5/lev)**(Rd/Cp))).T
-    taaf0 = ((fnc(taaMM, ns).T * (1.e5/lev)**(Rd/Cp))).T
-    zgcf0 = fnc(zgcMM, ns)
-    zgaf0 = fnc(zgaMM, ns)        
+        vacf0 = fnc(vacMM)
+        vaaf0 = fnc(vaaMM)
+        wacf0 = fnc(wacMM)
+        waaf0 = fnc(waaMM)
+    tacf0 = ((fnc(tacMM).T * (1.e5/lev)**(Rd/Cp))).T
+    taaf0 = ((fnc(taaMM).T * (1.e5/lev)**(Rd/Cp))).T
+    zgcf0 = fnc(zgcMM)
+    zgaf0 = fnc(zgaMM)        
 
     dbdycf0 = (9.81/tacf0.T[:,1:-1] * (tacf0.T[:,2:]-tacf0.T[:,:-2])).T/(R0*np.deg2rad(lat[2:]-lat[:-2]))[:,np.newaxis]
     dbdycf0 = np.ma.concatenate([np.nan*np.ones_like(dbdycf0.T[:,:1]), dbdycf0.T, np.nan*np.ones_like(dbdycf0.T[:,-1:])], axis=1).T
@@ -400,6 +606,74 @@ def read_datas_MMM(ybgn, yend, fnc, ns, models = None, cintm = False, dirbase = 
     else:
         return lon, lat, lev, uacf0, uaaf0, dbdycf0, dbdyaf0, dbdzcf0, dbdzaf0, egrcf0, egraf0
 
+
+# def read_datas_MMM(ybgn, yend, fnc, ns, models = None, cintm = False, dirbase = '/data16/theme-C/usijimay'):
+
+#     if models == None:
+#         models = read_models()
+
+#     model = 'EMS'+str(np.size(models))
+
+
+#     lon, lat, tosMM = ReadData2dMMClim('tos', models, 'historical', ybgn, yend, regrid = True, dirbase = dirbase)    
+#     if cintm:
+#         lono, lato, toso = rs.read_sst(ybgn, yend, dataname = 'input4MIPs', cann = False)        
+#     lev = np.array([1.e5, 9.25e4, 8.5e4, 7.e4, 6.e4, 5.e4, 4.e4, 3.e4, 2.5e4, 2.e4, 1.5e4, 1.e4, 7.e3, 5.e3, 3.e3, 2.e3, 1.e3, 5.e2, 1.e2])
+
+#     lonu, latu, levu, uacMM = ReadData3dMMClim('ua', models, 'historical', ybgn, yend, regrid = False, dirbase = dirbase, lon = lon, lat = lat, lev = lev)
+#     if cintm:        
+#         lonv, latv, levv, vacMM = ReadData3dMMClim('va', models, 'historical', ybgn, yend, regrid = False, dirbase = dirbase, lon = lon, lat = lat, lev = lev)
+#         lonw, latw, levw, wacMM = ReadData3dMMClim('wap', models, 'historical', ybgn, yend, regrid = False, dirbase = dirbase, lon = lon, lat = lat, lev = lev)
+#     lont, latt, levt, tacMM = ReadData3dMMClim('ta', models, 'historical', ybgn, yend, regrid = False, dirbase = dirbase, lon = lon, lat = lat, lev = lev)
+#     lont, latt, levt, zgcMM = ReadData3dMMClim('zg', models, 'historical', ybgn, yend, regrid = False, dirbase = dirbase, lon = lon, lat = lat, lev = lev)
+    
+#     lonu, latu, levu, uaaMM = ReadData3dMMClim('ua', models, 'amip', ybgn, yend, regrid = False, dirbase = dirbase, lon = lon, lat = lat, lev = lev)
+#     if cintm:        
+#         lonv, latv, levv, vaaMM = ReadData3dMMClim('va', models, 'amip', ybgn, yend, regrid = False, dirbase = dirbase, lon = lon, lat = lat, lev = lev)
+#         lonw, latw, levw, waaMM = ReadData3dMMClim('wap', models, 'amip', ybgn, yend, regrid = False, dirbase = dirbase, lon = lon, lat = lat, lev = lev)
+#     lont, latt, levt, taaMM = ReadData3dMMClim('ta', models, 'amip', ybgn, yend, regrid = False, dirbase = dirbase, lon = lon, lat = lat, lev = lev)
+#     lont, latt, levt, zgaMM = ReadData3dMMClim('zg', models, 'amip', ybgn, yend, regrid = False, dirbase = dirbase, lon = lon, lat = lat, lev = lev)
+    
+            
+#     R0 = 6.375e6
+#     Rd = 2.87e2
+#     Cp = 1.004e3
+            
+#     if cintm:            
+#         toscf0 = fnc(tosMM, ns)
+#         tosaf0 = fnc(toso, ns)
+#     uacf0 = fnc(uacMM, ns)
+#     uaaf0 = fnc(uaaMM, ns)
+#     if cintm:    
+#         vacf0 = fnc(vacMM, ns)
+#         vaaf0 = fnc(vaaMM, ns)
+#         wacf0 = fnc(wacMM, ns)
+#         waaf0 = fnc(waaMM, ns)
+#     tacf0 = ((fnc(tacMM, ns).T * (1.e5/lev)**(Rd/Cp))).T
+#     taaf0 = ((fnc(taaMM, ns).T * (1.e5/lev)**(Rd/Cp))).T
+#     zgcf0 = fnc(zgcMM, ns)
+#     zgaf0 = fnc(zgaMM, ns)        
+
+#     dbdycf0 = (9.81/tacf0.T[:,1:-1] * (tacf0.T[:,2:]-tacf0.T[:,:-2])).T/(R0*np.deg2rad(lat[2:]-lat[:-2]))[:,np.newaxis]
+#     dbdycf0 = np.ma.concatenate([np.nan*np.ones_like(dbdycf0.T[:,:1]), dbdycf0.T, np.nan*np.ones_like(dbdycf0.T[:,-1:])], axis=1).T
+    
+#     dbdyaf0 = (9.81/taaf0.T[:,1:-1] * (taaf0.T[:,2:]-taaf0.T[:,:-2])).T/(R0*np.deg2rad(lat[2:]-lat[:-2]))[:,np.newaxis]
+#     dbdyaf0 = np.ma.concatenate([np.nan*np.ones_like(dbdyaf0.T[:,:1]), dbdyaf0.T, np.nan*np.ones_like(dbdyaf0.T[:,-1:])], axis=1).T
+    
+#     dbdzcf0 = (9.81/tacf0.T[:,:,1:-1] * (tacf0.T[:,:,2:]-tacf0.T[:,:,:-2])/(zgcf0.T[:,:,2:]-zgcf0.T[:,:,:-2])).T
+#     dbdzcf0 = np.ma.concatenate([np.nan*np.ones_like(dbdzcf0.T[:,:,:1]), dbdzcf0.T, np.nan*np.ones_like(dbdzcf0.T[:,:,-1:])], axis=2).T
+    
+#     dbdzaf0 = (9.81/taaf0.T[:,:,1:-1] * (taaf0.T[:,:,2:]-taaf0.T[:,:,:-2])/(zgaf0.T[:,:,2:]-zgaf0.T[:,:,:-2])).T
+#     dbdzaf0 = np.ma.concatenate([np.nan*np.ones_like(dbdzaf0.T[:,:,:1]), dbdzaf0.T, np.nan*np.ones_like(dbdzaf0.T[:,:,-1:])], axis=2).T
+    
+#     egrcf0 = 0.31*np.abs(dbdycf0)/np.sqrt(np.maximum(dbdzcf0, 1.e-12)) * 86400.
+#     egraf0 = 0.31*np.abs(dbdyaf0)/np.sqrt(np.maximum(dbdzaf0, 1.e-12)) * 86400.
+
+#     if cintm:
+#         return lon, lat, lev, toscf0, tosaf0, uacf0, uaaf0, vacf0, vaaf0, wacf0, waaf0, tacf0, taaf0, zgcf0, zgaf0, dbdycf0, dbdyaf0, dbdzcf0, dbdzaf0, egrcf0, egraf0
+#     else:
+#         return lon, lat, lev, uacf0, uaaf0, dbdycf0, dbdyaf0, dbdzcf0, dbdzaf0, egrcf0, egraf0
+    
 def read_models():
     
     ampmdl = os.listdir('/Public/CMIPs/CMIP6/CMIP/amip/Amon/uas/')
